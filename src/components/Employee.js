0501b9bde8df110d13da4c62620f328ae6ed770d
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2024-01-21 08:08:43 +0530

    initial commit

---
+++ src/components/Employee.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Employee = ({ employee, deleteEmployee }) => {
+  return (
+    <tr key={employee.empId}>
+      <td className="text-left px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-500">{employee.firstName}</div>
+      </td>
+      <td className="text-left px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-500">{employee.lastName}</div>
+      </td>
+      <td className="text-left px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-500">{employee.emailID}</div>
+      </td>
+      <td className="text-right px-6 py-4 whitespace-nowrap font-medium text-sm">
+        <a href="#" className="text-indigo-600 hover:text-indigo-800 px-4">
+          Edit
+        </a>
+        <a
+          onClick={(e) => deleteEmployee(e, employee.empId)}
+          className="text-indigo-600 hover:text-indigo-800 hover:cursor-pointer"
+        >
+          Delete
+        </a>
+      </td>
+    </tr>
+  );
+};
+
+export default Employee;
---
+++ src/services/EmployeeService.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const EMPLOYEE_BASE_URL = "http://localhost:8090/api/v1/employee";
+
+class EmployeeService {
+  saveEmployee(employee) {
+    return axios.post(EMPLOYEE_BASE_URL, employee);
+  }
+
+  getEmployees() {
+    return axios.get(EMPLOYEE_BASE_URL);
+  }
+
+  async deleteEmployee(empId) {
+    try {
+      const response = await axios.delete(EMPLOYEE_BASE_URL + "/" + empId);
+      return response.data;
+    } catch (error) {
+      console.error("Error deleting employee:", error);
+      throw error;
+    }
+  }
+}
+
+export default new EmployeeService();
